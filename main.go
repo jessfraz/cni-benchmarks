c427d59544763921006a24222c3501ddea9603d6
Jess Frazelle <acidburn@microsoft.com>
2018-04-18 13:53:08 -0400

    update plugins
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- main.go
+++ main.go
@@ -51,79 +51,102 @@ func main() {
 		logrus.Fatalf("creating new CNI instance failed: %v", err)
 	}
 
-	// Create a new network namespace.
-	logrus.Info("Creating new netns process...")
-	cmd := exec.Command("sleep", "30")
-	cmd.SysProcAttr = &syscall.SysProcAttr{Unshareflags: syscall.CLONE_NEWNET}
-	if err := cmd.Start(); err != nil {
-		logrus.Fatalf("unsharing command failed: %v", err)
+	// Walk the configuration directory and get all the configs.
+	plugins := []string{}
+	if err := filepath.Walk(pluginConfDir, func(p string, f os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if f.IsDir() {
+			// Skip directories.
+			return nil
+		}
+
+		plugins = append(plugins, filepath.Base(strings.TrimSuffix(p, ".conf")))
+		return nil
+	}); err != nil {
+		logrus.Fatalf("walking plugin configuration directory %s failed: %v", pluginConfDir, err)
 	}
-	pid := cmd.Process.Pid
+	logrus.Infof("Found plugin configurations for %s", strings.Join(plugins, ", "))
 
 	logrus.Infof("Parent process ($this) has PID %d", os.Getpid())
-	logrus.Infof("New netns process has PID %d", pid)
 
-	// Load the CNI configuration.
-	if err := libcni.Load(
-		cni.WithLoNetwork,
-		cni.WithConfFile(filepath.Join(wd, "net.d", "macvlan.conf")),
-	); err != nil {
-		logrus.Fatalf("loading CNI configuration failed: %v", err)
-	}
+	// Iterate over the plugin configurations.
+	for _, plugin := range plugins {
+		// Create a new network namespace.
+		logrus.Infof("[%s] creating new netns process...", plugin)
+		cmd := exec.Command("sleep", "30")
+		cmd.SysProcAttr = &syscall.SysProcAttr{Unshareflags: syscall.CLONE_NEWNET}
+		if err := cmd.Start(); err != nil {
+			logrus.Fatalf("[%s] unsharing command failed: %v", plugin, err)
+		}
+		pid := cmd.Process.Pid
 
-	// Setup network for namespace.
-	//netnsFD := fmt.Sprintf("/proc/%d/task/%d/ns/net", os.Getpid(), syscall.Gettid())
-	netnsFD := fmt.Sprintf("/proc/%d/ns/net", pid)
-	result, err := libcni.Setup(fmt.Sprintf("%d", pid), netnsFD)
-	if err != nil {
-		logrus.Fatalf("setting up netns for id (%d) and netns (%s) failed: %v", pid, netnsFD, err)
-	}
+		logrus.Infof("[%s] new netns process has PID %d", plugin, pid)
 
-	// Get the IP of the default interface.
-	defaultInterface := cni.DefaultPrefix + "0"
-	ip := result.Interfaces[defaultInterface].IPConfigs[0].IP.String()
-	logrus.Infof("IP of the default interface in the netns is %s:%s", defaultInterface, ip)
+		// Load the CNI configuration.
+		if err := libcni.Load(
+			cni.WithLoNetwork,
+			cni.WithConfFile(filepath.Join(wd, "net.d", plugin+".conf")),
+		); err != nil {
+			logrus.Fatalf("[%s] loading CNI configuration failed: %v", plugin, err)
+		}
 
-	logrus.Infof("Getting netns file descriptor from the pid %d", pid)
-	newNS, err := netns.GetFromPid(pid)
-	if err != nil {
-		logrus.Fatalf("creating new netns failed: %v", err)
-	}
-	defer newNS.Close()
+		// Setup network for namespace.
+		//netnsFD := fmt.Sprintf("/proc/%d/task/%d/ns/net", os.Getpid(), syscall.Gettid())
+		netnsFD := fmt.Sprintf("/proc/%d/ns/net", pid)
+		result, err := libcni.Setup(fmt.Sprintf("%d", pid), netnsFD)
+		if err != nil {
+			logrus.Fatalf("[%s] setting up netns for id (%d) and netns (%s) failed: %v", plugin, pid, netnsFD, err)
+		}
 
-	// Switch into the new netns.
-	logrus.Infof("Performing setns into netns from pid %d", pid)
-	if err := netns.Set(newNS); err != nil {
-		logrus.Fatalf("switching to new netns failed: %v", err)
-	}
+		// Get the IP of the default interface.
+		defaultInterface := cni.DefaultPrefix + "0"
+		ip := result.Interfaces[defaultInterface].IPConfigs[0].IP.String()
+		logrus.Infof("[%s] IP of the default interface (%s) in the netns is %s", plugin, defaultInterface, ip)
 
-	// Get a list of the links.
-	links, err := netlink.LinkList()
-	if err != nil {
-		logrus.Fatalf("getting list of ip links failed: %v", err)
-	}
-	l := []string{}
-	for _, link := range links {
-		l = append(l, fmt.Sprintf("%s->%s", link.Type(), link.Attrs().Name))
-	}
-	if len(l) > 0 {
-		logrus.Infof("Found netns ip links: %s", strings.Join(l, ", "))
-	}
+		logrus.Infof("[%s] getting netns file descriptor from the pid %d", plugin, pid)
+		newNS, err := netns.GetFromPid(pid)
+		if err != nil {
+			logrus.Fatalf("[%s] creating new netns failed: %v", plugin, err)
+		}
+		defer newNS.Close()
 
-	// Try getting an outbound resource.
-	resp, err := http.Get("https://httpbin.org/ip")
-	if err != nil {
-		logrus.Errorf("getting an out of network resource failed: %v", err)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		logrus.Errorf("reading response body failed: %v", err)
-	}
-	logrus.Infof("httpbin returned: %s", strings.TrimSpace(string(body)))
+		// Switch into the new netns.
+		logrus.Infof("[%s] performing setns into netns from pid %d", plugin, pid)
+		if err := netns.Set(newNS); err != nil {
+			logrus.Fatalf("[%s] switching to new netns failed: %v", plugin, err)
+		}
 
-	// Switch back to the original netns.
-	logrus.Info("Switching back into our original netns...")
-	if err := netns.Set(originalNS); err != nil {
-		logrus.Fatalf("switching back to original netns failed: %v", err)
+		// Get a list of the links.
+		links, err := netlink.LinkList()
+		if err != nil {
+			logrus.Fatalf("[%s] getting list of ip links failed: %v", plugin, err)
+		}
+		l := []string{}
+		for _, link := range links {
+			l = append(l, fmt.Sprintf("%s->%s", link.Type(), link.Attrs().Name))
+		}
+		if len(l) > 0 {
+			logrus.Infof("[%s] found netns ip links: %s", plugin, strings.Join(l, ", "))
+		}
+
+		// Try getting an outbound resource.
+		resp, err := http.Get("https://httpbin.org/ip")
+		if err != nil {
+			logrus.Fatalf("[%s] getting an out of network resource failed: %v", plugin, err)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			logrus.Fatalf("[%s] reading response body failed: %v", plugin, err)
+		}
+		logrus.Infof("[%s] httpbin returned: %s", plugin, strings.Replace(strings.TrimSpace(string(body)), "\n", "", -1))
+
+		// Switch back to the original netns.
+		logrus.Info("Switching back into our original netns...")
+		if err := netns.Set(originalNS); err != nil {
+			logrus.Fatalf("[%s] switching back to original netns failed: %v", plugin, err)
+		}
 	}
 }
